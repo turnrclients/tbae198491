c9a9359228e8f86adafa6b20e22c2c4a1e2260d8
VistaClients <vista.clients@rapttechnologies.com>
2026-02-10 18:15:16 +0530

    css & js updated

--- assets/js/custom/editmode.js
+++ assets/js/custom/editmode.js
@@ -1,31 +1,42 @@
+
 $(document).ready(function () {
 
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+}
+
+$(document).ready(function () {
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+}
 
 
     $(document).on('click', '.edit-site', function() {
-        var filename = $(this).attr("href");
-        var requestFrom = $(this).attr("src");
-        // $(".selectedPageName").val(filename);
+     var filename = $(this).attr("href");
+        // alert('filename------------------: ' + filename);
+        console.log('filename: ', filename);
 
-        // alert(2222);
-         //  $(".formFieldFileName").val(filename);
-        // if($(".formFieldFileName").val()== undefined){
-        //     alert(111);
-        //     $(".formFieldFileName").val("index.html");
-        // }
-        // alert("tformext--------"+$(".formFieldFileName").val());
+        var clientName = getCookie('clientName');
+        // alert('clientName: ' + clientName);
 
-        var clientName = "";
-        var clientProjectName = "";
-        if (requestFrom == "dashboardPage") {
-            clientName = $("#editClientId").val();
-            clientProjectName = $("#editClientProjectId").val();
+        var clientProjectName = getCookie('projectName') || getCookie('clientProjectName');
+        // alert('clientProjectName: ' + clientProjectName);
+
+        if (filename){
+            console.log('requested file ------- ' + filename);
         } else {
-            clientName = getCookie('clientName');
-            clientProjectName = getCookie('projectName');
+            console.log('filename missing ---------- ');
+            return;
         }
-        // alert("fileName----"+ $(".formFieldFileName").val())
 
+        console.log('clientprojectname',clientProjectName)
         $.ajax({
             type: 'POST',
             url: "es/",
@@ -36,84 +47,24 @@ $(document).ready(function () {
             },
             headers: {
                  "X-Requested-With": "XMLHttpRequest",
-            //     "X-CSRFToken": getCookie("csrftoken"),
+                 "X-CSRFToken": getCookie("csrftoken"),
             },
             success: function (data) {
 
-
-                setCookie("clientName", clientName, 7);
-                setCookie("projectName", clientProjectName, 7);
-
-                var Slidercontent = $('<div>').html(data).find('.dynamic-slider-wrapper').html();
-                if (Slidercontent) {
-                    localStorage.setItem('dynamicSliderContent', Slidercontent);
-                }
-
-              var newTab = window.open("", "_self");
-              if (requestFrom == "dashboardPage") {
-                var newTab = window.open("test", "_blank");
-              }
-
-              newTab.document.write(data);
-              newTab.document.close();
-                newTab.onload = function () {
-                    function appendElement(tag, attributes, toBody) {
-                        var element;
-                if (tag === 'link') {
-                    element = newTab.document.querySelector('link[href="' + attributes.href + '"]');
-                    if (!element) {
-                        element = newTab.document.createElement(tag);
-                        for (var attr in attributes) {
-                            element[attr] = attributes[attr];
-                        }
-                        if (toBody) {
-                            newTab.document.body.appendChild(element);
-                        } else {
-                            newTab.document.head.appendChild(element);
-                        }
-                    }
-                } else if (tag === 'script') {
-                    element = newTab.document.querySelector('script[src="' + attributes.src + '"]');
-                    if (!element) {
-                        element = newTab.document.createElement(tag);
-                        for (var attr in attributes) {
-                            element[attr] = attributes[attr];
-                        }
-                        if (toBody) {
-                            newTab.document.body.appendChild(element);
-                        } else {
-                            newTab.document.head.appendChild(element);
-                        }
-                    }
-                }
-            }
+            //   var newTab = window.open("", "_self");
+            window.location.href =
+            `/es/?srcReq=${filename}`;
 
 
-              appendElement('link', { rel: 'stylesheet', href: 'https://cdn.quilljs.com/1.3.6/quill.snow.css' }, false);
-              appendElement('link', { rel: 'stylesheet', href: 'assets/css/custom/editmode.css' }, false);
-              appendElement('link', { rel: 'stylesheet', href: 'assets/css/custom/custom.css' }, false);
-              appendElement('script', { src: 'https://cdn.quilljs.com/1.3.6/quill.min.js', type: 'text/javascript' }, true);
-              appendElement('script', { src: 'assets/js/custom/main.js', type: 'text/javascript' }, true);
-              appendElement('script', { src: 'assets/js/custom/editmode.js', type: 'text/javascript' }, true);
-              appendElement('script', { src: 'assets/js/custom/editModeScript.js', type: 'text/javascript' }, true);
-
-              $('<input>', {type: 'hidden',class: 'hidden selectedPageName',name: 'selectedPageName',value: filename}).appendTo(newTab.document.body);
-
-
-                var anchorTags = newTab.document.querySelectorAll('a');
-                  anchorTags.forEach(function(anchor) {
-                  anchor.classList.add('edit-site');
-              });
-
-
-            };
             },
             error: function (data, errmsg, err) {
-                alert(data.responseJSON.errorMessage);
+                console.log(data.responseJSON.errorMessage);
             }
           });
           return false;
       });
+ });
+
  });
 
 
@@ -128,3 +79,173 @@ $(document).ready(function () {
 
 
 
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+
+//     function getCookie(name) {
+//         const value = `; ${document.cookie}`;
+//         const parts = value.split(`; ${name}=`);
+//         if (parts.length === 2) return parts.pop().split(';').shift();
+//         return null;
+//     }
+
+//     $(document).on('click', '.edit-site', function () {
+
+//         var filename = $(this).attr("href");
+//         console.log('filename: ', filename);
+
+//         if (!filename) {
+//             console.log('filename missing');
+//             return false;
+//         }
+
+//         var clientName = getCookie('clientName');
+//         var clientProjectName = getCookie('projectName') || getCookie('clientProjectName');
+
+//         console.log('clientprojectname', clientProjectName);
+
+//         // Cache bust token
+//         const CACHE_BUST = Date.now();
+
+//         $.ajax({
+//             type: 'POST',
+//             url: "es/",
+//             data: {
+//                 clientName: clientName,
+//                 clientProjectName: clientProjectName,
+//                 srcReq: filename
+//             },
+//             headers: {
+//                 "X-Requested-With": "XMLHttpRequest"
+//             },
+
+//             success: function (data) {
+//                 console.log('data: ', data);
+
+//                 var newTab = window.open("", "_self");
+
+//                 // Inject <base> with cache busting
+//                 const baseUrl =
+//                     `${window.location.protocol}//${window.location.host}/?_=${CACHE_BUST}`;
+
+//                 const updatedData = data.replace(
+//                     /<head([^>]*)>/i,
+//                     `<head$1><base href="${baseUrl}">`
+//                 );
+
+//                 newTab.document.open();
+//                 newTab.document.write(updatedData);
+//                 newTab.document.close();
+
+//                 newTab.onload = function () {
+
+//                     function cacheBust(url) {
+//                         return url.includes('?')
+//                             ? url + '&_=' + CACHE_BUST
+//                             : url + '?_=' + CACHE_BUST;
+//                     }
+
+//                     function appendElement(tag, attributes, toBody) {
+//                         var element;
+
+//                         if (tag === 'script') {
+//                             const bustedSrc = cacheBust(attributes.src);
+
+//                             element = newTab.document.querySelector(
+//                                 'script[src="' + bustedSrc + '"]'
+//                             );
+
+//                             if (!element) {
+//                                 element = newTab.document.createElement(tag);
+//                                 element.src = bustedSrc;
+//                                 element.type = attributes.type || 'text/javascript';
+
+//                                 if (toBody) {
+//                                     newTab.document.body.appendChild(element);
+//                                 } else {
+//                                     newTab.document.head.appendChild(element);
+//                                 }
+//                             }
+//                         }
+
+//                         if (tag === 'link') {
+//                             const bustedHref = cacheBust(attributes.href);
+
+//                             element = newTab.document.createElement(tag);
+//                             element.rel = attributes.rel;
+//                             element.href = bustedHref;
+
+//                             newTab.document.head.appendChild(element);
+//                         }
+//                     }
+
+//                     // Hidden input
+//                     $('<input>', {
+//                         type: 'hidden',
+//                         class: 'hidden selectedPageName',
+//                         name: 'selectedPageName',
+//                         value: filename
+//                     }).appendTo(newTab.document.body);
+
+//                     // Styles (cache-busted)
+//                     appendElement('link', {
+//                         rel: 'stylesheet',
+//                         href: 'https://cdn.quilljs.com/1.3.6/quill.snow.css'
+//                     }, false);
+
+//                     appendElement('link', {
+//                         rel: 'stylesheet',
+//                         href: 'assets/css/custom/editmode.css'
+//                     }, false);
+
+//                     appendElement('link', {
+//                         rel: 'stylesheet',
+//                         href: 'assets/css/custom/custom.css'
+//                     }, false);
+
+//                     // Scripts (cache-busted)
+//                     appendElement('script', {
+//                         src: 'https://cdn.quilljs.com/1.3.6/quill.min.js',
+//                         type: 'text/javascript'
+//                     }, true);
+
+//                     appendElement('script', {
+//                         src: 'assets/js/custom/main.js',
+//                         type: 'text/javascript'
+//                     }, true);
+
+//                     appendElement('script', {
+//                         src: 'assets/js/custom/editmode.js',
+//                         type: 'text/javascript'
+//                     }, true);
+
+//                     appendElement('script', {
+//                         src: 'assets/js/custom/editModeScript.js',
+//                         type: 'text/javascript'
+//                     }, true);
+
+//                     // Rebind edit links
+//                     var anchorTags = newTab.document.querySelectorAll('a');
+//                     anchorTags.forEach(function (anchor) {
+//                         anchor.classList.add('edit-site');
+//                     });
+//                 };
+//             },
+
+//             error: function (data) {
+//                 alert(data.responseJSON?.errorMessage || "Error");
+//             }
+//         });
+
+//         return false;
+//     });
+// });
